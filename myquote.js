edeb335231749568411c387c814f45a7abf42d62
jintaap <jintaap@users.noreply.github.com>
2016-08-10 21:43:26 -0400

    Update myquote.js

--- myquote.js
+++ myquote.js
@@ -2,21 +2,18 @@ var looms = ["Jaquard-loom", "poet", "sculptor", "primadonna"];
 var nouns = ["flowers and leaves", "images and emotions", "figures and statues", "melodies"];
 var verbs = ["weaves", "evokes", "crafts", "sings"];
 
-function choosei(lst){
-    var i = Math.floor(Math.random() * lst.length);
+function choosei(){
+    var i = Math.floor(Math.random() * 4);
     return i;
 }
 
-function choose(lst){
-    return lst[choosei(lst)];
-}
 
-var pick = choosei(looms);
+var pick = choosei();
 var loom = looms[pick];
 var flowers = nouns[pick];
 var weaves = verbs[pick];
 
 var q = document.getElementById("quote");
 
-q.innerHTML = "\"The Analytical Engine" + weaves + "algebraic patterns, just as the " + loom + " " + weaves + " " + " " + stuff + "".\"";
+q.innerHTML = "\"The Analytical Engine" + weaves + "algebraic patterns, just as the " + loom + " " + weaves + " " + " " + flowers + "".\"";
 q.innerHTML += "<br><br><br>";
